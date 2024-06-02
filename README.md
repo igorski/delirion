7c4b87e7861e4444d8353972111e9a5062e910e7
Igor Zinken <igor.zinken@gmail.com>
2024-06-02 18:51:49 +0200

    Restructured CMake file

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Doppler
 
-Doppler was built using the [JUCE framework](https://github.com/juce-framework/JUCE)*.
-
-_*The version used to create this plugin was [7.0.12](https://github.com/juce-framework/JUCE/releases/tag/7.0.12)_
+Doppler was built using the [JUCE framework](https://github.com/juce-framework/JUCE).
 
 ## Setup
 
-You will need to have CMake and a suitable C compiler installed.
+You will need to have CMake and a suitable C compiler installed. JUCE* is a submodule of this repository so
+all library dependencies are handled upon cloning this repository.
 
-+ Clone this repository
-+ Clone the JUCE framework into a sub folder called `JUCE` (note it is ignored)
+_*The version used to create this plugin was [7.0.12](https://github.com/juce-framework/JUCE/releases/tag/7.0.12)_
+
+You can create a runtime by running the below:
+
+```
+cmake . -B build -G <GENERATOR_RUNTIME>
+```
+
+where the G flag is optional (default to common generator for your platform) and provided `<GENERATOR_RUNTIME>` should reflect a custom generator to use (e.g. XCode on Mac, "Unix makefiles", Ninja, etc.)
---
+++ src/PluginEditor.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Igor Zinken https://www.igorski.nl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    AudioPluginAudioProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
