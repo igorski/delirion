b1716ee10221ea2879fa1624f6e9b47471a0b82a
Igor Zinken <730069+igorski@users.noreply.github.com>
2026-02-05 07:31:57 +0100

    Updated build info and shell scripts

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ run the following to clone the JUCE framework into a subfolder `JUCE`, relative 
 git clone https://github.com/juce-framework/JUCE
 ```
 
-_*The version used to create this plugin was [8.0.1](https://github.com/juce-framework/JUCE/releases/tag/8.0.1)_
+_*The version used to create this plugin was [8.0.10](https://github.com/juce-framework/JUCE/releases/tag/8.0.10)_
 
 ### Build
 
@@ -37,4 +37,18 @@ After which you can run:
 
 ```
 cmake --build
+```
+
+### Signing the plugin on macOS
+
+You will need to have your code signing set up appropriately. Assuming you have set up your Apple Developer account, you can find your signing identity like so:
+
+```
+security find-identity -p codesigning -v 
+```
+
+From which you can take your name and team id (the code between parentheses) and pass them to the build script like so:
+
+```
+sh build.sh --team_id TEAM_ID --identity "YOUR_NAME"
 ```
--- build.sh
+++ build.sh
@@ -2,10 +2,51 @@
 clear
 echo "Flushing build caches and output folders"
 rm -rf build
+
 echo "Creating build folders"
 mkdir build
 cd build
+
+# Parse arguments
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --team_id) team_id="$2"; shift ;;
+        --identity) identity="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 echo "Building project"
 echo "----------------"
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
+
+buildStatus=$?
+
+if [ $buildStatus -eq 0 ]; then
+
+    echo "Plugin built successfully"
+
+    # code signing (macOS)
+
+    echo "Signing plugins..."
+    
+    if [ "$identity" ]; then
+        
+        # Audio Unit
+        AU_FILE="./delirion_artefacts/Release/AU/Delirion.component"
+        codesign -s "${identity}" "${AU_FILE}" --timestamp --deep --strict --options=runtime --force
+        codesign --verify --deep --verbose "${AU_FILE}"
+
+        # VST
+        VST_FILE="./delirion_artefacts/Release/VST3/Delirion.vst3"
+        codesign -s "${identity}" "${VST_FILE}" --timestamp --deep --strict --options=runtime --force
+        codesign --verify --deep --verbose "${VST_FILE}"
+    fi
+else
+    echo "An error occurred during build of plugin"
+fi
+
+exit $buildStatus
